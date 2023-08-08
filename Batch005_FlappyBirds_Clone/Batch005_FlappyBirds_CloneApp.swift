4b488bbabc74a085bad9ba609d5c2a1aa886fb93
Till-Hemmerich-Syntax <till.hemmerich@syntax-institut.de>
2023-08-08 10:58:18 +0200

    Initial Commit

---
+++ Batch005_FlappyBirds_Clone/Batch005_FlappyBirds_CloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  Batch005_FlappyBirds_CloneApp.swift
+//  Batch005_FlappyBirds_Clone
+//
+//  Created by Till Hemmerich on 08.08.23.
+//
+
+import SwiftUI
+
+@main
+struct Batch005_FlappyBirds_CloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
